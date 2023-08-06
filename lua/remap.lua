663bc22f2e2e79fc061c6f6f8f310152e5e66423
hash <66149987+antarktidi4@users.noreply.github.com>
2023-08-06 19:02:31 +0700

    Add error message keybind

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader><tab>', vim.cmd.Ex)
 vim.keymap.set('i', '<C-f>', '<Esc>')
+vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
